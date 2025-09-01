48b80ce7b738aaf27dc031358ee0f523b855f735
Shoumik <k.s.mujtaheed@gmail.com>
2025-09-01 11:57:53 +0600

    vite fix for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // This should match Vercel's Output Directory setting
+  },
   server: {
     proxy: {
       // Proxy API requests starting with /api to your backend server:
